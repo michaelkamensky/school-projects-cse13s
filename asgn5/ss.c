6168003702676766c9d581f98814631baff79b33
Michael Kamensky <mkamensk@ucsc.edu>
2023-02-18 19:31:40 +0000

    got ss_make_pub to work not sure if it has bugs

--- asgn5/ss.c
+++ asgn5/ss.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 
 #include "ss.h"
+#include "numtheory.h"
 
 //
 // Export SS public key to output stream
@@ -37,4 +38,51 @@ void ss_read_pub(mpz_t n, char username[], FILE *pbfile) {
     gmp_fscanf(pbfile, "%Zd\n", n); 
     fscanf(pbfile, "%s\n", username);
 }
+
+//
+// Generates the components for a new SS key.
+//
+// Provides:
+//  p:  first prime
+//  q: second prime
+//  n: public modulus/exponent
+//
+// Requires:
+//  nbits: minimum # of bits in n
+//  iters: iterations of Miller-Rabin to use for primality check
+//  all mpz_t arguments to be initialized
+//
+void ss_make_pub(mpz_t p, mpz_t q, mpz_t n, uint64_t nbits, uint64_t iters) {
+    mpz_t q_1, p_1, pp, mod1, mod2;    
+    mpz_inits( q_1, p_1, pp, mod1, mod2, NULL);
+    uint64_t upper = ((2*nbits)/5);
+    uint64_t lower = (nbits/5);
+    // generates a random value from lower to upper
+    uint64_t p_bits = (rand() % (upper - lower + 1)) + lower;
+    uint64_t q_bits = nbits - (2*p_bits);
+    while (true) {
+        // make p prime
+        make_prime(p, p_bits, iters);
+        // make q prime
+        make_prime(q, q_bits, iters);
+        // p_1 = p - 1
+        mpz_sub_ui(p_1, p, 1);
+        // q_1 = q - 1
+        mpz_sub_ui(q_1, q, 1);
+        // mod1 = p % q_1
+        mpz_mod(mod1, p, q_1);
+        // mod2 = q % p_1
+        mpz_mod(mod2, q, p_1);
+        // if ((mod1 != 0) and (mod2 != 0))
+        if ((mpz_cmp_ui(mod1, 0) != 0) && (mpz_cmp_ui(mod2, 0) != 0)) {
+            break;
+        }
+    }
+    // pp = p^2
+    mpz_pow_ui(pp, p, 2);
+    // n = pp * q
+    mpz_mul(n, pp, q);
+    mpz_clears( q_1, p_1, pp, mod1, mod2, NULL);
+}
+
 
--- asgn5/ss_ut.c
+++ asgn5/ss_ut.c
@@ -9,6 +9,8 @@ void test_ss_write_pub(uint32_t av, char username[], FILE *f) {
     mpz_inits(n, NULL);
     mpz_set_ui(n, av);
     ss_write_pub(n, username, f);
+    mpz_clears(n, NULL);
+
 
 }
 
@@ -19,8 +21,19 @@ void test_ss_read_pub(FILE *f) {
     ss_read_pub(n, username, f);
     gmp_printf("%Zd\n", n);
     printf("%s\n", username);
+    mpz_clears(n, NULL);
 
 }
+
+void test_ss_make_pub(uint64_t nbits, uint64_t iters) {
+    mpz_t n, p, q;
+    mpz_inits(n, p, q, NULL);
+    ss_make_pub(p, q, n, nbits, iters);
+    gmp_printf("n = %Zd, p = %Zd, q = %Zd\n", n, p, q);
+    mpz_clears(n, p, q, NULL);
+
+}
+
 
 int main(int argc, char **argv) {
     FILE *f_out = fopen("test_out.txt", "w");
@@ -29,6 +42,12 @@ int main(int argc, char **argv) {
     FILE *f_in = fopen("test_in.txt", "r");
     test_ss_read_pub(f_in);
     fclose(f_in);
+    randstate_init(1);
+    test_ss_make_pub(30, 10);
+    test_ss_make_pub(256, 20);
+    test_ss_make_pub(1024, 30);
+    test_ss_make_pub(2048, 100);
+    randstate_clear();
 
 #if 0
     test_pow_mod(111, 17, 137);
