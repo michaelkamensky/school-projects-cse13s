e2d568c991a486dcd550b021e5a8a46a5fcb4d65
Michael Kamensky <mkamensk@ucsc.edu>
2023-03-04 01:42:46 +0000

    finished the read and write of the io.c

--- asgn6/io.c
+++ asgn6/io.c
@@ -10,6 +10,11 @@ uint64_t total_syms;
 uint64_t total_bits;
 
 int read_bytes(int infile, uint8_t *buf, int to_read) {
+    // check if the infile if valid
+    if (infile == -1) {
+        printf("There was an error in read_bytes faulty file int was passed");
+        return 0;
+    }
     ssize_t bytes_read;
     int total_read = 0;
     while (1) {
@@ -23,4 +28,25 @@ int read_bytes(int infile, uint8_t *buf, int to_read) {
         }
     }
     return total_read;
+}
+
+int write_bytes(int outfile, uint8_t *buf, int to_write) {
+    // check if the outfile if valid
+    if (outfile == -1) {
+        printf("There was an error in write_bytes faulty file int was passed");
+        return 0;
+    }
+    ssize_t bytes_writen;
+    int total_wrote = 0;
+    while (1) {
+        bytes_writen = write(outfile, buf + total_wrote, to_write - total_wrote);
+        total_wrote += bytes_writen;
+        if (bytes_writen == 0) {
+            break;
+        }
+        if (total_wrote == to_write) {
+            break;
+        }
+    }
+    return total_wrote;
 }
--- asgn6/io_ut.c
+++ asgn6/io_ut.c
@@ -14,6 +14,12 @@ int main(void) {
     int in_file_int = open(in_file_name, O_RDONLY);
     int read_this_bytes = 1000;
     uint8_t *buf = (uint8_t *) calloc(read_this_bytes, sizeof(uint8_t));
-    int bytes = read_bytes(in_file_int, buf, read_this_bytes);
+    int bytes_r = read_bytes(in_file_int, buf, read_this_bytes);
+
+    // testing the write capabilities
+    char *out_file_name = "test_write.txt";
+    int out_file_int = open(out_file_name, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    int write_this_bytes = 50;
+    int bytes_w = write_bytes(out_file_int, buf, write_this_bytes); 
     return 0;
 }
