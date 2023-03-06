a7c4a47b57735354e2bf5d3db6c5aef0a4939a8b
Michael Kamensky <mkamensk@ucsc.edu>
2023-03-06 19:04:58 +0000

    updating the .h files to offical

--- asgn6/io.h
+++ asgn6/io.h
@@ -2,18 +2,18 @@
 #define __IO_H__
 
 #include "word.h"
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#define BLOCK 4096 // 4KB blocks.
+#define BLOCK 4096       // 4KB blocks.
 #define MAGIC 0xBAADBAAC // Unique encoder/decoder magic number.
 
 extern uint64_t total_syms; // To count the symbols processed.
 extern uint64_t total_bits; // To count the bits processed.
 
 typedef struct FileHeader {
-    uint32_t magic;
-    uint16_t protection;
+  uint32_t magic;
+  uint16_t protection;
 } FileHeader;
 
 int read_bytes(int infile, uint8_t *buf, int to_read);
--- asgn6/trie.h
+++ asgn6/trie.h
@@ -8,20 +8,49 @@
 typedef struct TrieNode TrieNode;
 
 struct TrieNode {
-    TrieNode *children[ALPHABET];
-    uint16_t code;
+  TrieNode *children[ALPHABET];
+  uint16_t code;
 };
 
-TrieNode *trie_node_create(uint16_t index);
+/*
+ * Creates a new TrieNode and returns a pointer to it
+ * Allocate memory for TrieNode
+ * Code is the code to be assigned to this new node
+ * Returns the newly allocated node
+ */
+TrieNode *trie_node_create(uint16_t code);
 
+/*
+ * Deletes Node n
+ * Frees any allocated memory
+ */
 void trie_node_delete(TrieNode *n);
 
+/*
+ * Constructor: Creates the root TrieNode and returns a pointer to it
+ * Allocate memory for TrieNode
+ * Code is START_CODE
+ * Returns the newly allocated node
+ */
 TrieNode *trie_create(void);
 
+/*
+ * Resets the trie: called when code reaches MAX_CODE
+ * Deletes all the children of root and frees allocated memory
+ */
 void trie_reset(TrieNode *root);
 
+/*
+ * Destructor: Deletes all nodes starting at n as the root
+ * Deletes any children recursively before deleting the root
+ * Frees all the memory allocated for TrieNodes n and below
+ */
 void trie_delete(TrieNode *n);
 
+/*
+ * Checks if node has any children called sym
+ * Returns the address if found, NULL if absent
+ */
 TrieNode *trie_step(TrieNode *n, uint8_t sym);
 
 #endif
